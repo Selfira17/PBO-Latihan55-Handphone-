2cec5f2893c82c991efe544ae2591a2ab62388f1
selfira <selfiraayw@gmail.com>
2021-04-01 20:00:12 +0700

    PBO Latihan55 Handphone

---
+++ src/pbo_latihan55_handphone/PBO_Latihan55_Handphone.java
@@ -0,0 +1,78 @@
+/*
+ * Nama    : Selfira Anyuniarwati
+Kelas   : PBO (FS112B)
+NIM     : 2031332802
+Deskripsi : Latihan 55-Handphone
+ */
+package pbo_latihan55_handphone;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO_Latihan55_Handphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        
+     String namamanufaktur = " Samsung";
+     String namaOS = "Android";
+     String Modelapa = "Grand";
+     int Harganya = 3000000;
+     String keynya = "234ibfd3840fo";
+     
+     
+     System.out.println("Manufaktur   :"+namamanufaktur);
+     System.out.println("OS           :"+namaOS);
+     System.out.println("Model        :"+Modelapa);
+     System.out.println("Harga        :"+Harganya);
+     System.out.println("Android Key Store   :"+keynya);
+     
+     
+     System.out.println("=======================================================");
+     
+     
+     String manufaktur = "BlackB";
+     String OS = "RIM";
+     String Modelnya = "Curve";
+     int harga = 2000000;
+     String pinya = "BHS249";
+        
+     System.out.println("Manufaktur     :"+manufaktur);
+     System.out.println("OS             :"+OS);
+     System.out.println("Model          :"+Modelnya);
+     System.out.println("Harga          :"+harga);
+     System.out.println("PIN            :"+pinya);
+     
+     
+     System.out.println("=======================================================");
+     
+     
+     String manufaktur1 = "Nokia";
+     String OS1 = "Win8";
+     String Model1 = "Lumia";
+     int harga1 = 1000000;
+     String WP = "UUUQIJWORJ";
+     
+     
+     System.out.println("Manufaktur     :"+manufaktur1);
+     System.out.println("OS             :"+OS1);
+     System.out.println("Model          :"+Model1);
+     System.out.println("Harga          :"+harga1);
+     System.out.println("PIN            :"+WP);
+     
+     
+     
+    
+     
+     
+    
+    
+    
+    }
+    
+}
